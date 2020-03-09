106f77dd02fd3c0b05a6d7832798e95ea813f1ab
sm-1357 <2353404m@student.gla.ac.uk>
2020-03-09 20:57:58 +0000

    Basic homepage set up

--- stack_underflow/views.py
+++ stack_underflow/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Welcome to StackUnderflow")
+
+    context_dict = {'boldmessage': 'Hello'}
+    return render(request, 'stack_underflow/index.html', context=context_dict)
