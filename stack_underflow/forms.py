5f997aed0c1b94ff6a7d6277ff6810498ef837cd
“Paul <2205382t@student.gla.ac.uk>
2020-03-10 21:33:01 +0000

    updated models.py

--- stack_underflow/forms.py
+++ stack_underflow/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from rango.models import UserProfile
+from stack_underflow.models import UserProfile
 
 class Sign_Up_Form(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
--- stack_underflow/models.py
+++ stack_underflow/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+class User(models.Model):
+    #Anything else needed here?
+    loggedIn = models.BooleanField()#default=false?
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    userName = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.userName
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    threads = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Thread(models.Model):
+    #FOREIGN KEY?
+    question = models.CharField(max_length=128)
+    replies = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.question
+
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    text = models.CharField(max_length=2000)
+
+class Notification(models.Model):
+    userProfile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    reply = models.ForeignKey(Reply, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
+
+
+
+
